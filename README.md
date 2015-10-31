3317aeb759357cf7591344b124b0c87d8d0b1e62
Kai Curry <kai@cape.io>
2015-10-30 22:29:04 -0500

    init release 0.0.1

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
 # rethinkdb-template
 
 Ensure RethinkDB contains databases, tables, and indexes defined in template.
+
+Something like this:
+
+```
+var r = require('rethinkdbdash')();
+var applyDbTemplate = require('rethinkdb-template');
+
+var template = [
+  {
+    db: 'demo',
+    tables: [
+      {
+        table: 'user',
+        indexes: [
+          {
+            index: 'ids',
+            value: {
+              multi: true,
+            },
+          },
+        ],
+      },
+      {
+        table: 'provider',
+      },
+    ],
+  },
+];
+
+rethinkQuery = applyDbTemplate({template: template, r: r});
+rethinkQuery.run().then(...);
+
+```
---
+++ src/index.js
@@ -0,0 +1,59 @@
+import { isObject, map } from 'lodash';
+
+// Step 4.
+// Create index if it's missing.
+function createIndex({r, db, table, dbIndexes, index, value}) {
+  return {
+    index,
+    value: isObject(value) ? value : '[function]',
+    result: r.branch(
+      dbIndexes.contains(index),
+      true,
+      r.db(db).table(table).indexCreate(index, value)
+    ),
+  };
+}
+
+// Step 3.
+// Create table if it's missing.
+function createTable({r, dbTables, db, table, indexes}) {
+  function processIndexes(dbIndexes) {
+    return map(indexes, info => createIndex({r, db, table, dbIndexes, ...info}));
+  }
+  return r({
+    table,
+    result: r.branch(
+      dbTables.contains(table), true, r.db(db).tableCreate(table)
+    ),
+  }).merge({
+    indexes: r.db(db).table(table).indexList().do(processIndexes),
+  });
+}
+
+// Step 2.
+// Create table if it's missing.
+function createDb({r, dbs, db, tables}) {
+  function processTables(dbTables) {
+    return map(tables, info => createTable({r, db, dbTables, ...info}));
+  }
+  return r({
+    db,
+    result: r.branch(dbs.contains(db), true, r.dbCreate(db)),
+  }).merge({
+    tables: r.db(db).tableList().do(processTables),
+  });
+}
+
+// Step 1.
+// Get list of databases and send to Step 2.
+// template is an array of objects that descibe databases.
+function checkTemplate({r, template}) {
+  function processDbs(dbs) {
+    // Take our input db info and send off for processing.
+    return map(template, info => createDb({r, dbs, ...info}));
+  }
+  // Get list of databases from RethinkDB.
+  return r.dbList().do(processDbs);
+}
+
+export default checkTemplate;
